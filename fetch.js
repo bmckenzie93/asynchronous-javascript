b66443c64351427dd8bae77ae1bc28f4d33020c0
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2020-02-11 20:18:29 -0800

    Fetch Json COMPLETE

--- fetch.js
+++ fetch.js
@@ -6,6 +6,30 @@ function getText() {
       return response.text();
     })
     .then(function(data) {
-      console.log(data);
+      const p = document.createElement('p');
+      p.textContent = data;
+      document.getElementById('output').appendChild(p);
+    })
+    .catch(function(error) {
+      console.log(error);
     });
+}
+
+document.getElementById('getJson').addEventListener('click', getJson);
+
+function getJson() {
+  fetch('customers.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      data.forEach(function(customer) {
+        document.getElementById('output').innerHTML += `
+          <ul><li><h1>${customer.id}</h1></li><li>${customer.name}</li><li>${customer.age}</li></ul>
+        `
+      })
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
 }
